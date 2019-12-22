f52ade741f3ce3b1ea70f64a7678bb4ea5a6bdaf
PeterBrain <peter.loecker@live.at>
2019-12-22 12:41:41 +0100

    serializer & fixtures

--- projman/serializers.py
+++ projman/serializers.py
@@ -5,7 +5,7 @@ from projman import models
 class DepartmentFormSerializer(serializers.ModelSerializer):
     class Meta:
         model = models.Department
-        fields = ['id', 'name', 'description']
+        fields = '__all__'
 
 
 class EmployeeListSerializer(serializers.ModelSerializer):
@@ -16,16 +16,16 @@ class EmployeeListSerializer(serializers.ModelSerializer):
         fields = ['id', 'first_name', 'last_name', 'dob', 'department']
 
     def get_department(self, obj):
-        return obj.department.name if obj.department else ''
+        return obj.department.name + ' - ' + obj.department.description if obj.department else ''
 
 
 class EmployeeFormSerializer(serializers.ModelSerializer):
     class Meta:
         model = models.Employee
-        fields = ['id', 'first_name', 'last_name', 'dob', 'department'] #'__all__'
+        fields = '__all__'
 
 
 class ProjectFormSerializer(serializers.ModelSerializer):
     class Meta:
         model = models.Project
-        fields = ['id', 'name', 'description', 'start_date', 'end_date', 'progress', 'finished']
+        fields = '__all__'
